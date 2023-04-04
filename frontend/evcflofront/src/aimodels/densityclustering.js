aa9ff2113632ac5cadac2c1a2167a5fa447de2d9
BarryCILs <xhbarry@gmail.com>
2023-04-04 14:29:50 +1000

    1. create aimodels folder to house all AI models from researchs 2. create EVCS densityclustering web page under aimodels folder to house the AI models

---
+++ frontend/evcflofront/src/aimodels/densityclustering.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const DensityClustering = () => {
+  return (
+    <div>
+      <h1> EVCS density clustering model</h1>
+      <p className="centerText"> EV Charging Station Density Clustering Model</p>
+        <div>
+        </div>
+    </div>
+  );
+};
+
+export default DensityClustering;
