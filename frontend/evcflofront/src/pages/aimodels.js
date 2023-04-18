c496140825f76e296dc56e15fef27df9c2457e06
BarryCILs <xhbarry@gmail.com>
2023-04-18 14:14:58 +1000

    add stylish and create table on the AI models page

--- frontend/evcflofront/src/pages/aimodels.js
+++ frontend/evcflofront/src/pages/aimodels.js
@@ -1,20 +1,55 @@
 const AImodels = () => {
   return (
     <div className="aiModels">
-      <h1>AI models PAGE</h1>
+      
       <p className="centerText">
+      <h1>AI/ML EVCS applications</h1>
         {" "}
-        This page contains all Artificial Intelligence Models that generated
+        This page contains all Artificial Intelligence/Machine Learning Model application that generated
         from our researchs to help promote EV adoption
       </p>
       <div class="centered">
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href={process.env.PUBLIC_URL + "densityclusteringmap.html"}
-        >
-          Density Clustering Map
-        </a>
+        <table width="70%" className="centerText">
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Application</th>
+          </tr>
+          {/* 1st model */}
+          <tr>
+            <td>EVCS density clustering model map</td>
+            <td>EVCS locations display on the map with colours showing their density in selected area</td>
+            <td>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={process.env.PUBLIC_URL + "densityclusteringmap.html"}
+              >
+                Density Clustering Map
+              </a>
+            </td>
+          </tr>
+          {/* 2nd model */}
+          <tr>
+            <td>Reserve</td>
+            <td>Reserve</td>
+            <td>
+              Reserve
+            </td>
+          </tr>
+          
+        </table>
+        <ul>
+          {/* <li>
+              <a
+              target="_blank"
+              rel="noreferrer"
+              href={process.env.PUBLIC_URL + "densityclusteringmap.html"}
+            >
+              Density Clustering Map
+            </a>
+          </li> */}
+        </ul>
       </div>
     </div>
   );
