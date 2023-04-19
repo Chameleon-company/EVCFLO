f21bb376465b07732c29c21ccd9a2e9a94c3e237
BarryCILs <xhbarry@gmail.com>
2023-04-19 14:51:37 +1000

    add and link EVCS cluster model to AImodel table

--- frontend/evcflofront/src/pages/aimodels.js
+++ frontend/evcflofront/src/pages/aimodels.js
@@ -1,14 +1,14 @@
+import { Link } from "react-router-dom";
+
 const AImodels = () => {
   return (
     <div className="aiModels">
-      
       <p className="centerText">
-      <h1>AI/ML EVCS applications</h1>
-        {" "}
-        This page contains all Artificial Intelligence/Machine Learning Model application that generated
-        from our researchs to help promote EV adoption
+        <h1>AI/ML EVCS applications</h1> This page contains all Artificial
+        Intelligence/Machine Learning Model application that generated from our
+        researchs to help promote EV adoption
       </p>
-      
+
       <div class="centered">
         <table width="80%" className="centerText">
           <tr>
@@ -19,7 +19,15 @@ const AImodels = () => {
           {/* 1st model */}
           <tr>
             <td>EVCS density clustering model map</td>
-            <td>EVCS locations display on the map with colours showing their density in selected area</td>
+            <td>
+              VIC and QLD EVCS locations display on the map with colours showing
+              their density in selected area
+              <ur>
+                <li>Red Icon: EVCS concentrated location</li>
+                <li>Yellow Icon: EVCS less concentrated location</li>
+                <li>Green Icon: EVCS locations where is not concentrated</li>
+              </ur>
+            </td>
             <td>
               <a
                 target="_blank"
@@ -34,26 +42,39 @@ const AImodels = () => {
           <tr>
             <td>Future EVCS Recommendation Map</td>
             <td>
-              Showing colour icons of different EVCS in Melbourne and Geelong areas
+              Showing colour icons of different EVCS in Melbourne and Geelong
+              areas
               <ur>
                 <li>Red Icon: EVCS near another EVCS within 5km radius</li>
                 <li>Green Icon: EVCS has no other EVCS within 5km radius</li>
-                <li>Blue Icon: Recommended future EVCS locations for carpark having over 100 parking spaces </li>
+                <li>
+                  Blue Icon: Recommended future EVCS locations for carpark
+                  having over 100 parking spaces{" "}
+                </li>
               </ur>
             </td>
             <td>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={process.env.PUBLIC_URL + "EVCSrecommendationsmap.html"}
-            >
-              Click me
-            </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={process.env.PUBLIC_URL + "EVCSrecommendationsmap.html"}
+              >
+                Click me
+              </a>
             </td>
           </tr>
-          
+          {/* 3rd model */}
+          <tr>
+            <td>EVCS clustering prediction model</td>
+            <td>
+              Applied Machine learning models to predict EVCS clustering based
+              on input data
+            </td>
+            <td>
+              <Link to="/densityclustering">Click me</Link>
+            </td>
+          </tr>
         </table>
-
       </div>
     </div>
   );
