76bd975700bf7b21dd3805405c6000f3d0ddd6ce
MarkPremier <markpremier@gmail.com>
2023-05-04 13:28:20 +1000

    changed some CSS

--- frontend/evcflofront/src/data.js
+++ frontend/evcflofront/src/data.js
@@ -1,8 +1,7 @@
 export const pageLinks = [
   { id: 1, href: "/home", text: "home " },
-  { id: 2, href: "/datapage", text: "data " },
-  { id: 3, href: "/research", text: "research " },
-  { id: 4, href: "/aimodels", text: "AI models " }
+  { id: 2, href: "/research", text: "research " },
+  { id: 3, href: "/aimodels", text: "AI models " },
 ];
 
 export const socialLinks = [
@@ -15,12 +14,13 @@ export const socialLinks = [
 
 // These show if the user isn't authenticated
 export const notAuthenticatedLinks = [
-  { id: 5, href: "/login", text: "Login " },
-  { id: 6, href: "/signup", text: "Signup " }
-]
+  { id: 4, href: "/login", text: "Login " },
+  { id: 5, href: "/signup", text: "Signup " },
+];
 
 // These show if the user is authenticated
 export const authenticatedLinks = [
   { id: 6, href: "/user", text: "User " },
-  { id: 7, href: "/logout", text: "Logout " }
-]
+  { id: 7, href: "/datapage", text: "data " },
+  { id: 8, href: "/logout", text: "Logout " },
+];
