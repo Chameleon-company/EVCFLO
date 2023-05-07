82741d5c2d5d90f5e01bfe82315911d495bc89f5
BarryCILs <xhbarry@gmail.com>
2023-05-07 10:10:36 +1000

    link the notebooks data to the research page

--- frontend/evcflofront/src/pages/research.js
+++ frontend/evcflofront/src/pages/research.js
@@ -1,9 +1,13 @@
 import React from "react";
+// import Searchbar from "../components/NotebookSearchBar";
+import NotebookTable from "../components/NotebookTable";
 
 const Research = () => {
   return (
     <div>
       <h1>RESEARCH PAGE</h1>
+      {/* <Searchbar /> */}
+      <NotebookTable />
     </div>
   );
 };
