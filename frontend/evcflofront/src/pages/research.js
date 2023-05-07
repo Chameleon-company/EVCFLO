a1d823ac89f41761f660f095b16b803fc351dc24
MarkPremier <markpremier@gmail.com>
2023-05-07 21:26:11 +1000

    Changed some formatting

--- frontend/evcflofront/src/pages/research.js
+++ frontend/evcflofront/src/pages/research.js
@@ -1,11 +1,9 @@
-import React from "react";
 // import Searchbar from "../components/NotebookSearchBar";
 import NotebookTable from "../components/NotebookTable";
 
 const Research = () => {
   return (
     <div>
-      <h1>RESEARCH PAGE</h1>
       {/* <Searchbar /> */}
       <NotebookTable />
     </div>
