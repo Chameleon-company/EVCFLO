08ce8a0d760228b3c46568699c7c5b4735430bc3
MarkPremier <markpremier@gmail.com>
2023-05-11 10:22:50 +1000

    chnaged data page

--- frontend/evcflofront/src/data.js
+++ frontend/evcflofront/src/data.js
@@ -20,7 +20,7 @@ export const notAuthenticatedLinks = [
 
 // These show if the user is authenticated
 export const authenticatedLinks = [
-  { id: 6, href: "/user", text: "User " },
   { id: 7, href: "/datapage", text: "data " },
+  { id: 6, href: "/user", text: "User " },
   { id: 8, href: "/logout", text: "Logout " },
 ];
