22a0aa643031b04a8673db971f8ad2dea3e0f5cb
MarkPremier <markpremier@gmail.com>
2023-05-24 20:16:29 +1000

    Removed bootstrap references

--- frontend/evcflofront/src/index.js
+++ frontend/evcflofront/src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "boxicons/css/boxicons.min.css";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
